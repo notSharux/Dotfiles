9d4c9b4d41b37de98b5f50b99281a6be1049065d
notSharux <85288797+notSharux@users.noreply.github.com>
2021-06-24 21:07:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo pacman -S xmonad xmonad-contrib qtile alacritty xterm nitrogen git base-dev
 sudo chown +x dofiles/install.sh
 
 # yay -S otf-font-awesome-5-free
+# sudo pacman -S ttf-hack-3.003-3
+# sudo pacman -S ttf-joypixels-6.5.0-1
 
 # git clone https://aur.archlinux.org/yay
 # cd yay
