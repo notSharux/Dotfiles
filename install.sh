8d9a9b5dfcd2aef6e40d78fcf5aff3b9c6f6af02
notSharux <85288797+notSharux@users.noreply.github.com>
2021-06-24 15:53:07 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S xmonad xmonad-contrib qtile alacritty xterm nitrogen git base-devel pcmanfm firefox xmobar lxappearance vim xarchiver
+
+# git clone https://aur.archlinux.org/yay
+# cd yay
+# makepkg -si
+
+# git clone https://github.com/jonaburg/picom
+# cd picom
+# makepkg -sii
+# picom --experimental-backend --config ~/.config/picom.conf
