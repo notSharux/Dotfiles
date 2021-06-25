edf743c0c51058aa6dc07d74e275d5391e06d962
notSharux <85288797+notSharux@users.noreply.github.com>
2021-06-25 20:07:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S xmonad xmonad-contrib qtile alacritty xterm nitrogen git base-devel pcmanfm firefox xmobar lxappearance vim xarchiver bspwm sxhkd pulseaudio pavucontrol
+sudo pacman -S xmonad xmonad-contrib qtile alacritty xterm nitrogen git base-devel pcmanfm firefox xmobar lxappearance vim xarchiver bspwm sxhkd pulseaudio pavucontrol starship
 
 
 # yay -S otf-font-awesome-5-free
