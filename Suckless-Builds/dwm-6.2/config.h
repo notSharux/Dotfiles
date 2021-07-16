a6336abee052bfd327a86a50b29865c14a037d21
notSharux <85288797+notSharux@users.noreply.github.com>
2021-07-16 20:32:05 +0200

    Update config.h

--- Suckless-Builds/dwm-6.2/config.h
+++ Suckless-Builds/dwm-6.2/config.h
@@ -60,12 +60,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *emacscmd[]  = { "emacs", NULL };
+static const char *firefoxcmd[] = { "firefox", NULL};
+static const char *surfcmd[] = { "tabbed", "surf", "-e", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_e,      spawn,          {.v = emacscmd } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = surfcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 /*	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },          */
