52bf0f9ddc467545c6c71ab4be22a5800834469b
notSharux <85288797+notSharux@users.noreply.github.com>
2021-07-18 15:08:32 +0200

    Update config.h

--- Suckless-Builds/dmenu-5.0/config.h
+++ Suckless-Builds/dmenu-5.0/config.h
@@ -12,7 +12,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#253858" },
 	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
 	[SchemeNormHighlight] = { "#a5bdf8", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
