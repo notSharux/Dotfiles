c7dd60afd1cd625639c700624ef126929d7b20da
notSharux <85288797+notSharux@users.noreply.github.com>
2021-07-18 22:44:50 +0200

    Update time.sh

--- Scripts/time.sh
+++ Scripts/time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ls /usr/share/zoneinfo/
-rm /etc/localtime
-ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
+sudo rm /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
