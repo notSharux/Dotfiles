a6f61c242fbd344feb65fae2ae5803b2978a99b3
notSharux <85288797+notSharux@users.noreply.github.com>
2021-08-11 16:25:11 +0200

    Update blocks.h

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"   ", "/home/tobi/.config/dwmblocks/memory_gb",	5,		1},
+	{"   ", "~/.config/dwmblocks/memory_gb",	5,		1},
 
 	/*	{"", "/home/tobi/.config/dwmblocks/memory_percentage",	5,		0}, */
 
-	{"|   ", "/home/tobi/.config/dwmblocks/cpu_percentage",	2,		1},
-	{"|    ", "/home/tobi/.config/dwmblocks/packages",  2,		25},
-	{"|     ", "/home/tobi/.config/dwmblocks/pacupdate",  2,		25},
+	{"|   ", "~/.config/dwmblocks/cpu_percentage",	2,		1},
+	{"|    ", "~/.config/dwmblocks/packages",  2,		25},
+	{"|     ", "~/.config/dwmblocks/pacupdate",  2,		25},
 
 	/*{"", "/home/tobi/.config/dwmblocks/cpu_bars",	2,		0},      */
 	/*{"| ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
 
-	{"|   ",     "/home/tobi/.config/dwmblocks/kernel",	360,		360},
-	{"| ",     "/home/tobi/.config/dwmblocks/clock",	15,		1},
+	{"|   ",     "~/.config/dwmblocks/kernel",	360,		360},
+	{"| ",     "~/.config/dwmblocks/clock",	15,		1},
 	{" ",     "",	                                 0,		0},
 };
 
